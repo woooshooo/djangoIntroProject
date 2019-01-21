b254486aa26b2dc6ac9ba1b5525831af60de2ed6
Webster <kaykaygenise@gmail.com>
2019-01-21 18:07:45 +0800

    initial commit

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello world!')
+    context = {}
+    questions = Question.objects.all()
+    context['questions'] = questions
+    return render(request, 'index.html', context)
 
 def help(request):
     return HttpResponse('help page')
